9c3e1f9ec003469ad083efc44ae4fd5b3da798f8
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2023-03-27 01:26:59 +0300

    Update matrixs.hpp

---
+++ balabanova_k_a/prj.lab/matrixs/include/matrixs/matrixs.hpp
@@ -0,0 +1,16 @@
+#include<ostream>
+#include<cstdeff>
+
+class MatrixS {
+private:
+	int** matrix_ = 0;
+	int cols_ = 0;
+	int rows_ = 0;
+
+public:
+	explicit MatrixS();
+	explicit MatrixS(int rows, int cols);
+	MatrixS(const MatrixS& matrix);
+	MatrixS operetor=(const MatrixS& matrix);
+	~MatrixS();
+};
