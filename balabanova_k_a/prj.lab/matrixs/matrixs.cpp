5fa8238cda92ef3567380015f400e8fccf9d11c6
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2023-03-27 01:17:21 +0300

    Update matrixs.cpp

---
+++ balabanova_k_a/prj.lab/matrixs/matrixs.cpp
@@ -0,0 +1,71 @@
+#include <matrixs/matrixs.hpp>
+
+explicit MatrixS::MatrixS() {
+	cols = 0;
+	rows = 0;
+	matrix_ = nullptr;
+}
+
+explicit MatrixS::MatrixS(int rows, int cols) {
+	rows_ = rows;
+	cols_ = cols;
+	matrix_ = (int**) new int* [rows_];
+	for (int i = 0; i < rows_; ++i) {
+		matrix_[i] = int* new int[cols_];
+	}
+	//мы заполняем массив нулями или он остается пустым?
+
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			matrix_[i][j] = 0;
+		}
+	}
+}
+
+MatrixS::MatrixS(const MatrixS& matrix) {
+	rows_ = matrix.rows_;
+	cols_ = matrix.cols_;
+	matrix_ = (int**) new (int*)[rows_];
+	for (int i = 0; i < rows_; ++i) {
+		matrix_[i] = (int*) new int[cols_];
+	}
+	for (int i = 0; i < rows_; ++i) {
+		for (int j = 0; j < cols_; ++j) {
+			matrix_[i][j] = matrix[i][j];
+		}
+	}
+}
+
+MatrixS::MatrixS operetor=(const MatrixS& matrix){
+		if (cols_ > 0) {
+			for (int i = 0; i < rows_; ++i) {
+				delete[] matrix_[i];
+			}
+		}
+		if (rows_ > 0) {
+			delete[] matrix_;
+		}
+		rows_ = matrix.rows_;
+		cols_ = matrix.cols_;
+		matrix_ = (int**) new (int*)[rows_];
+		for (int i = 0; i < rows_; ++i) {
+			matrix_[i] = (int*) new int[cols_];
+		}
+		for (int i = 0; i < rows_; ++i) {
+			for (int j = 0; j < cols_; ++j) {
+				matrix_[i][j] = matrix[i][j];
+			}
+		}
+		return *this;
+}
+
+~MatrixS() {
+	if (cols_ > 0) {
+		for (int i = 0; i < rows_; ++i) {
+			delete[] matrix_[i];
+		}
+	}
+	if (rows_ > 0) {
+		delete[] matrix_;
+	}
+}
