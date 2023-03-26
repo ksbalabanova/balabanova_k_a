71616966377edc304d4ca69d04de60b7b4d1f3d5
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2023-03-27 01:14:56 +0300

    Update arrayd.cpp

--- balabanova_k_a/prj.lab/arrayd/arrayd.cpp
+++ balabanova_k_a/prj.lab/arrayd/arrayd.cpp
@@ -1,5 +1,5 @@
 #include <arrayd/arrayd.hpp>
-
+//remove don't work
 void ArrayD::resize(std::ptrdiff_t size) {
 	if (size > ssize_) {
 		int capacity = 0;
@@ -13,16 +13,20 @@ void ArrayD::resize(std::ptrdiff_t size) {
 		for (int i = 0; i < ssize_; ++i) {
 			data[i] = data_[i];
 		}
+		for (int i = ssize_; i < size; ++i) {
+			data[i] = 0.0;
+		}
 		delete[] data_;
 		data_ = data;
 		capacity_ = capacity;
 	}
+	
 	ssize_ = size;
 }
 
 double& ArrayD::operator[](std::ptrdiff_t i) {
 	if (i < 0 || ssize_ < i) {
-		throw;
+		throw std::invalid_argument("index does not exist");
 	}
 	return data_[i];
 }
@@ -35,6 +39,9 @@ ArrayD::ArrayD() {
 	ssize_ = 0;
 	data_ = nullptr;
 	capacity_ = 0;
+	for (int i = 0; i < ssize_; ++i) {
+		data_[i] = 0.0;
+	}
 }
 
 ArrayD::ArrayD(const ArrayD& rhs) {
@@ -60,6 +67,9 @@ ArrayD::ArrayD(const std::ptrdiff_t size) {
 	else {
 		data_ = 0;
 	}
+	for (int i = 0; i < ssize_; ++i) {
+		data_[i] = 0.0;
+	}
 }
 
 ArrayD ArrayD::operator=(const ArrayD& rhs) {
@@ -76,4 +86,66 @@ ArrayD ArrayD::operator=(const ArrayD& rhs) {
 }
 ArrayD::~ArrayD(){
 	delete[] data_;
+}
+
+void ArrayD::insert(const std::ptrdiff_t& i, const double& x) {
+	if (i < 0 || i > ssize_) {
+		throw std::invalid_argument("index doesn't exist");
+	}
+	ssize_ += 1;
+	double* old = data_;
+	data_ = new double[ssize_];
+	for (int j = 0; j < i; ++j) {
+		data_[j] = old[j];
+	}
+	data_[i] = x;
+	for (int j = i + 1; j < ssize_; ++j) {
+		data_[j] = old[j - 1];
+	}
+	delete[] old;
+}
+
+void ArrayD::remove(const int& i) {
+	if (i < ssize_) {
+		if (ssize_ > 1) {
+			for (int j = i; j < ssize_ - 1; ++j) {
+				data_[j] = data_[j + 1];
+			}
+		}
+	}
+}
+
+std::istream& ArrayD::ReadFrom(std::istream& istrm) {
+	if (ssize_ > 0) {
+		int i = 0;
+		double xi = 0;
+		while (!istrm.eof() && istrm.good() && i < ssize_) {
+			istrm >> xi;
+			data_[i] = xi;
+			i++;
+		}
+	}
+	else {
+		throw std::exception();
+	}
+	return istrm;
+}
+
+std::ostream& ArrayD::WriteTo(std::ostream& ostrm) {
+	if (ssize_ > 0) {
+		int i = 0;
+		while (ostrm.good() && i < ssize_) {
+			ostrm << data_[i] << " ";
+			i++;
+		}
+	}
+	return ostrm;
+}
+
+std::ostream& operator<<(std::ostream& ostrm, ArrayD& array) {
+	return array.WriteTo(ostrm);
+}
+
+std::istream& operator>>(std::istream& istrm, ArrayD& array) {
+	return array.ReadFrom(istrm);
 }
