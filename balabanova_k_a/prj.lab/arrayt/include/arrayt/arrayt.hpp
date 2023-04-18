4833960ade4d91188c2b7157836e347f6c472752
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2023-04-18 22:11:56 +0300

    Update arrayt.hpp

--- balabanova_k_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
+++ balabanova_k_a/prj.lab/arrayt/include/arrayt/arrayt.hpp
@@ -4,14 +4,14 @@
 template<typename T>
 class ArrayT{
 private:
-	std::ptrdiff_t ssize_ = 0;
+	std::ptrdiff_t size_ = 0;
 	T* data_ = nullptr;
 	int capacity_ = 0;
 
 public:
 	ArrayT() = default;
 	ArrayT(const ArrayT& rhs);
-	ArrayT(const std::ptrdiff_t size = 0);
+	ArrayT(const std::ptrdiff_t size);
 	T operator=(const ArrayT<T>& rhs);
 	~ArrayT();
 
@@ -29,45 +29,44 @@ public:
 
 template<typename T>
 ArrayT<T>::ArrayT<T>(const ArrayT<T>& rhs) {
-	ssize_ = rhs.ssize_;
-	data_ = rhs.data_;
+	size_ = rhs.size_;
 	capacity_ = rhs.capacity_;
-	if (ssize_ != 0) {
-		data_ = new T[ssize_];
+	if (capacity_ > 0) {
+		data_ = new T[capacity_];
+		for (std::ptrdiff_t i = 0; i < size_; ++i) {
+			data_[i] = rhs.data_[i];
+		}
 	}
 	else {
-		data_ = 0;
-	}
-	for (int i = 0; i < ssize_; ++i) {
-		data_[i] = rhs.data_[i];
+		data_ = nullptr;
 	}
 }
 
 template<typename T>
 ArrayT<T>::ArrayT<T>(const std::ptrdiff_t size) {
-	ssize_ = size;
-	capacity_ = size;
-	if (ssize_ != 0) {
-		data_ = new T[ssize_];
+	if (size < 0) {
+		throw std::invalid_argument("Size of the new array must be greater than 0");
 	}
-	else {
-		data_ = 0;
-	}
-	for (int i = 0; i < ssize_; ++i) {
-		data_[i] = 0.0;
+	if (size_ > 0) {
+		resize(size);
 	}
 }
 
 template<typename T>
-T ArrayT<T>::operator=(const ArrayT& rhs) {
+T ArrayT<T>::operator=(const ArrayT<T>& rhs) {
 	if (this != &rhs) {
 		delete[] data_;
-		ssize_ = rhs.ssize_;
-		capacity_ = rhs.capacity_;
-		data_ = new T[capacity_];
-		for (int i = 0; i < capacity_; ++i) {
-			data_[i] = rhs.data_[i];
+		size_ = 0;
+		capacity_ = 0;
+		data_ = nullptr;
+		if (rhs.capacity_ > 0) {
+			data_ = new T[rhs.capacity_];
+			capacity_ = rhs.capacity_;
+			for (std::ptrdiff_t i = 0; i < capacity_; ++i) {
+				data_[i] = rhs.data_[i];
+			}
 		}
+		size_ = rhs.size_;
 	}
 	return *this;
 }
@@ -80,7 +79,7 @@ ArrayT<T>::~ArrayT<T>() {
 
 template<typename T>
 [[nodiscard]] const T& ArrayT<T>::operator[](std::ptrdiff_t i) const {
-	if (i < 0 || ssize_ < i) {
+	if (i < 0 || i >= size_) {
 		throw std::invalid_argument("index does not exist");
 	}
 	return data_[i];
@@ -88,7 +87,7 @@ template<typename T>
 
 template<typename T> 
 [[nodiscard]] T& ArrayT<T>::operator[](const std::ptrdiff_t i) {
-	if (i < 0 || ssize_ < i) {
+	if (i < 0 || i >= size_) {
 		throw std::invalid_argument("index does not exist");
 	}
 	return data_[i];
@@ -106,65 +105,67 @@ void ArrayT<T>::resize(const std::ptrdiff_t size) {
 	if (size <= 0) {
 		throw std::invalid_argument("New size must be greater than 0");
 	}
-	if (size > ssize_) {
-		std::ptrdiff_t capacity = 0;
-		if (size >= ssize_ * 2) {
-			capacity = size;
-		}
-		else {
-			capacity = ssize_ * 2;
-		}
-		T* data = new T[capacity];
-		for (int i = 0; i < ssize_; ++i) {
+	if (size <= size_) {
+		size_ = size;
+		return;
+	}
+	if (size > capacity_) {
+		T* data = new T[size] {};
+		for (size_t i = 0; i < size_; ++i) {
 			data[i] = data_[i];
-		}
-		for (int i = ssize_; i < size; ++i) {
-			data[i] = 0.0;
 		}
 		delete[] data_;
 		data_ = data;
-		capacity_ = capacity;
-		ssize_ = size;
+		size_ = size;
+		capacity_ = size * 2;
+		return;
+	}
+	if (size <= capacity_) {
+		size_ = size;
 	}
 }
 
 template<typename T>
 void ArrayT<T>::insert(const std::ptrdiff_t& i, const T& x) {
-	if (i < 0 || i > ssize_) {
+	if (i < 0 || i > size_) {
 		throw std::invalid_argument("index doesn't exist");
 	}
-	ssize_ += 1;
-	T* old = data_;
-	data_ = new T[ssize_];
-	for (int j = 0; j < i; ++j) {
-		data_[j] = old[j];
+	if (size_ == capacity_) {
+		resize(size_ + 1);
 	}
-	data_[i] = x;
-	for (int j = i + 1; j < ssize_; ++j) {
-		data_[j] = old[j - 1];
+	if (size_ == 0) {
+		data_[0] = x;
 	}
-	delete[] old;
+	else {
+		for (ptrdiff_t j = size_ - 1; j >= i; --j) {
+			data_[j + 1] = data_[j];
+		}
+		data_[i] = x;
+	}
+	size_++;
 }
 
 template <typename T> 
 void ArrayT<T>::remove(const int& i) {
-	if (i < ssize_) {
-		if (ssize_ > 1) {
-			for (int j = i; j < ssize_ - 1; ++j) {
-				data_[j] = data_[j + 1];
-			}
-		}
-		ssize_ -= 1;
+	if (size_ == 0) {
+		return;
 	}
+	if (i < 0 || i >= size_) {
+		throw std::invalid_argument("index does not exist");
+	}
+	for (int j = i; j < size_ - 1; ++j) {
+		data_[j] = data_[j + 1];
+	}
+	size_--;
 }
 
 
 template <typename T> 
 std::istream& ArrayT<T>::ReadFrom(std::istream& istrm) {
-	if (ssize_ > 0) {
+	if (size_ > 0) {
 		int i = 0;
 		T xi = 0;
-		while (!istrm.eof() && istrm.good() && i < ssize_) {
+		while (!istrm.eof() && istrm.good() && i < size_) {
 			istrm >> xi;
 			data_[i] = xi;
 			i++;
@@ -178,9 +179,9 @@ std::istream& ArrayT<T>::ReadFrom(std::istream& istrm) {
 
 template <typename T> 
 std::ostream& ArrayT<T>::WriteTo(std::ostream& ostrm) {
-	if (ssize_ > 0) {
+	if (size_ > 0) {
 		int i = 0;
-		while (ostrm.good() && i < ssize_) {
+		while (ostrm.good() && i < size_) {
 			ostrm << data_[i] << " ";
 			i++;
 		}
