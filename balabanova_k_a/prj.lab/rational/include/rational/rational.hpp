a925ce74e7fdab92a8cfd186fbb6e7f685fc66fc
ksbalabanova <114703175+ksbalabanova@users.noreply.github.com>
2023-04-21 09:20:19 +0300

    Update rational.hpp

--- balabanova_k_a/prj.lab/rational/include/rational/rational.hpp
+++ balabanova_k_a/prj.lab/rational/include/rational/rational.hpp
@@ -3,7 +3,7 @@
 #include<exception>
 #include<stdexcept>
 
-int32_t GCD(int32_t a, int32_t b);
+int32_t nod(int32_t a, int32_t b);
 
 class Rational {
 public:
@@ -12,56 +12,53 @@ public:
 	Rational(const int32_t n, const int32_t d);
 	explicit Rational(const int32_t n) noexcept;
 	~Rational() = default;
-	Rational operator=(const Rational& rhs);
 
-	Rational operator+(const int x);
-	Rational operator-(const int x);
-	Rational operator*(const int x);
-	Rational operator/(const int x);
-
-	Rational operator-();
-	Rational& operator++();
-	Rational& operator--();
-
-
-	Rational& operator+=(const int x);
-	Rational& operator-=(const int x);
-	Rational& operator*=(const int x);
-	Rational& operator/=(const int x);
 	Rational& operator+=(const Rational& rhs);
 	Rational& operator-=(const Rational& rhs);
 	Rational& operator*=(const Rational& rhs);
 	Rational& operator/=(const Rational& rhs);
 
-	bool const operator==(const Rational& rhs) const noexcept;
-	bool const operator!=(const Rational& rhs) const noexcept;
-	bool const operator>(const Rational& rhs) const noexcept;
-	bool const operator<(const Rational& rhs) const noexcept;
-	bool const operator<=(const Rational& rhs) const noexcept;
-	bool const operator>=(const Rational& rhs) const noexcept;
+	Rational& operator+=(const int32_t & rhs);
+	Rational& operator-=(const int32_t & rhs);
+	Rational& operator*=(const int32_t & rhs);
+	Rational& operator/=(const int32_t & rhs);
+
+	Rational& operator-();
+	Rational& operator++();
+	Rational& operator--();
+	Rational operator++(int);
+	Rational operator--(int);
+
+	bool operator==(const Rational& rhs) const noexcept;
+	bool operator!=(const Rational& rhs) const noexcept;
+	bool operator>(const Rational& rhs) const noexcept;
+	bool operator<(const Rational& rhs) const noexcept;
+	bool operator<=(const Rational& rhs) const noexcept;
+	bool operator>=(const Rational& rhs) const noexcept;
 
 	std::ostream& writeTo(std::ostream& ostrm) const noexcept;
 	std::istream& readFrom(std::istream& istrm)noexcept;
 
 
 private:
-	int32_t num_ = 0 ;
-	int32_t denum_ = 1;
+	int32_t num_{0};
+	int32_t denum_{1};
 	static const char separator{ '/' };//формат
 };
 
-std::istream& operator>>(std::istream& istrm, Rational& rhs);
 std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs);
-
-Rational operator+(const Rational& rhs, const Rational& lhs);
-Rational operator-(const Rational& rhs, const Rational& lhs);
-Rational operator*(const Rational& rhs, const Rational& lhs);
-Rational operator/(const Rational& rhs, const Rational& lhs);
+std::istream& operator>>(std::istream& istrm, Rational& rhs);
 
 
-bool const operator==(const Rational& lhs, const int x);
-bool const operator!=(const Rational& lhs, const int x);
-bool const operator>(const Rational& lhs, const int x);
-bool const operator<(const Rational& lhs, const int x);
-bool const operator<=(const Rational& lhs, const int x);
-bool const operator>=(const Rational& lhs, const int x);
+Rational operator+(const Rational& lhs, const Rational& rhs);
+Rational operator-(const Rational& lhs, const Rational& rhs);
+Rational operator*(const Rational& lhs, const Rational& rhs);
+Rational operator/(const Rational& lhs, const Rational& rhs);
+
+
+bool operator==(const Rational &lhs, const int32_t &rhs);
+bool operator!=(const Rational &lhs, const int32_t &rhs);
+bool operator>(const Rational &lhs, const int32_t &rhs);
+bool operator<(const Rational &lhs, const int32_t &rhs);
+bool operator<=(const Rational &lhs, const int32_t &rhs);
+bool operator>=(const Rational &lhs, const int32_t &rhs);
